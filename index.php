0902b807b065e6403658ec7c78eff464a0ab54e5
Hunter Fuller <hfuller@pixilic.com>
2015-04-26 22:35:03 -0500

    handle when the length parameter is present but is empty-string

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once("burg.include.php");
-if ( array_key_exists("length",$_REQUEST) ) {
+if ( array_key_exists("length",$_REQUEST) and strlen($_REQUEST["length"] > 0 ) ) {
 	$length = intval($_REQUEST["length"]);
 	$burg = burg($length);
 } else {
